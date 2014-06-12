09f790894aaf1ec78522bd05e1815b0d240d7f3c
csb324 <csb324@gmail.com>
2014-06-12 18:38:20 -0400

    Testing CarLot --- it works! yay!

--- bin/testing.rb
+++ bin/testing.rb
@@ -1,16 +1,14 @@
 require 'pry'
 require_relative '../lib/used_car.rb'
 require_relative '../lib/damage.rb'
+require_relative '../lib/car_lot.rb'
 
 newzola = Car.new("Volvo", "Red", 2007, 30000, 1500)
 puts newzola.value
 
 oldzola = UsedCar.new("Volvo", "Red", 2007, 30000, 1500)
-puts oldzola.value
 
 oldzola.add_mileage(14000)
-puts oldzola.value
-puts oldzola.price
 
 scrape = Damage.new(1000, "I might have driven into a curb.")
 currently_on_fire = Damage.new(10000, "This car has been on fire for a while now...")
@@ -18,7 +16,13 @@ currently_on_fire = Damage.new(10000, "This car has been on fire for a while now
 oldzola.add_damage(scrape)
 oldzola.add_damage(currently_on_fire)
 
-puts oldzola.value
+puts newzola.price
 puts oldzola.price
 
-binding.pry
+best_lot_ever = CarLot.new("such an amazing lot")
+
+best_lot_ever.add_car(newzola)
+puts best_lot_ever.value
+
+best_lot_ever.add_car(oldzola)
+puts best_lot_ever.value
