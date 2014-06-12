3573c7cb2efad9f52bb999231ae7b9c763b65ba9
csb324 <csb324@gmail.com>
2014-06-12 18:29:51 -0400

    Set my car on fire. Oops!!

--- bin/testing.rb
+++ bin/testing.rb
@@ -10,10 +10,15 @@ puts oldzola.value
 
 oldzola.add_mileage(14000)
 puts oldzola.value
+puts oldzola.price
 
 scrape = Damage.new(1000, "I might have driven into a curb.")
+currently_on_fire = Damage.new(10000, "This car has been on fire for a while now...")
 
 oldzola.add_damage(scrape)
+oldzola.add_damage(currently_on_fire)
+
 puts oldzola.value
+puts oldzola.price
 
 binding.pry
